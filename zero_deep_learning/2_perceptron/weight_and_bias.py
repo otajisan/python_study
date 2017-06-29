b54acc8b3249ad1964db41befa344a9b523f10a1
otajisan <taji.taji.otajisan@gmail.com>
2017-06-29 12:14:31 +0900

    XOR回路による多層パーセプトロンのサンプル

--- zero_deep_learning/2_perceptron/weight_and_bias.py
+++ zero_deep_learning/2_perceptron/weight_and_bias.py
@@ -2,11 +2,11 @@
 
 import numpy as np
 
-def run_perceptron(x1, x2, weight=0.5, bias=-0.7):
+def run_simple_perceptron(x1, x2, weight=0.5, bias=-0.7):
     '''
     重みとbias
+    デフォルトはAND回路
     '''
-    print('### run perceptron ###')
     x = np.array([x1, x2])
     w = np.array([weight, weight])
     print(w*x)
@@ -14,6 +14,45 @@ def run_perceptron(x1, x2, weight=0.5, bias=-0.7):
     print(float(np.sum(w*x) + bias))
 
     return 1 if float(np.sum(w*x) + bias) > 0 else 0
+
+def AND(x1, x2):
+    '''
+    AND回路
+    '''
+    print('### AND ###')
+    return run_simple_perceptron(x1, x2)
+
+
+def NAND(x1, x2):
+    '''
+    NAND回路
+    '''
+    print('### NAND ###')
+    w = -0.5
+    b = 0.7
+    return run_simple_perceptron(x1, x2, weight=w, bias=b)
+
+
+def OR(x1, x2):
+    '''
+    OR回路
+    '''
+    print('### OR ###')
+    w = 0.5
+    b = -0.2
+    return run_simple_perceptron(x1, x2, weight=w, bias=b)
+
+
+def XOR(x1, x2):
+    '''
+    XOR回路(多層パーセプトロン)
+    '''
+    print('### XOR ###')
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    print("s1={s1} s2={s2} y={y}".format(s1=s1, s2=s2, y=y))
+    return y
 
 
 if __name__ == '__main__':
@@ -25,18 +64,20 @@ if __name__ == '__main__':
     ]
     print(">>>>>>>> AND回路")
     for data in input_data:
-        print(run_perceptron(data[0], data[1]))
+        print(AND(data[0], data[1]))
 
     print(">>>>>>>> NAND回路")
-    w = -0.5
-    b = 0.7
     for data in input_data:
-        print(run_perceptron(data[0], data[1], weight=w, bias=b))
+        print(NAND(data[0], data[1]))
 
     print(">>>>>>>> OR回路")
-    w = 0.5
-    b = -0.2
     for data in input_data:
-        print(run_perceptron(data[0], data[1], weight=w, bias=b))
+        print(OR(data[0], data[1]))
     # XOR回路
-    # -> 非線形でしか分類できないので、単純パーセプトロンではムリ！
+    # -> 非線形でしか分類できないので、一層のみの単純パーセプトロンではムリ！
+    # なので、「多層パーセプトロン」で実現する
+    # ここでは、AND x NAND x ORゲート(単純パーセプトロン)をつなげて
+    # XOR回路(多層パーセプトロン)を実現する
+    print(">>>>>>>> XOR回路")
+    for data in input_data:
+        print(XOR(data[0], data[1]))
