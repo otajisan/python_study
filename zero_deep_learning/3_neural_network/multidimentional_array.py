a0449dd4e0881c6ead46324cbc8f3fe823062f08
otajisan <taji.taji.otajisan@gmail.com>
2017-07-13 12:19:19 +0900

    3.4章の終わりまで

--- zero_deep_learning/3_neural_network/multidimentional_array.py
+++ zero_deep_learning/3_neural_network/multidimentional_array.py
@@ -50,6 +50,7 @@ def run_neural_network_dot_product():
 
     # 3.4 3層のニューラルネットワークの実装
     print('=== 3層のニューラルネットワークの実装 ====')
+    # 1層目
     X = np.array([1.0, 0.5]) # 入力値
     W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]) # 重み
     B1 = np.array([0.1, 0.2, 0.3]) # バイアス
@@ -57,12 +58,67 @@ def run_neural_network_dot_product():
     Z1 = sigmoid(A1) # 活性化関数としてシグモイド関数を利用
     print(Z1)
     print_chart(A1, Z1)
+    # 2層目
+    W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    B2 = np.array([0.1, 0.2])
+    A2 = np.dot(Z1, W2) + B2
+    Z2 = sigmoid(A2)
+    print(Z2)
+    print_chart(A2, Z2)
+    # 3層目
+    W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+    B3 = np.array([0.1, 0.2])
+    A3 = np.dot(Z2, W3) + B3
+    Y = identity_function(A3)
+    print(Y)
+    print_chart(A3, Y)
+
+def init_network():
+    '''
+    重みとバイアスの初期化
+    '''
+    network = {}
+    # 1層目
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['B1'] = np.array([0.1, 0.2, 0.3])
+    # 2層目
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['B2'] = np.array([0.1, 0.2])
+    # 3層目
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['B3'] = np.array([0.1, 0.2])
+
+    return network
+
+def forward(network, x):
+    '''
+    入力から出力への伝達処理
+    '''
+    # 重み
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    # バイアス
+    b1, b2, b3 = network['B1'], network['B2'],network['B3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
 
 def sigmoid(x):
     '''
     シグモイド関数
     '''
     return 1 / (1 + np.exp(-x))
+
+def identity_function(x):
+    '''
+    恒等関数(入力をそのまま出力する関数：σ(シグマ))
+    '''
+    return x
 
 def print_chart(x, y):
     '''
@@ -77,3 +133,9 @@ if __name__ == '__main__':
     run_simple()
     run_dot()
     run_neural_network_dot_product()
+    # まとめ
+    print('=== まとめ ===')
+    network = init_network()
+    x = np.array([1.0, 0.5]) # 入力値
+    y = forward(network, x)
+    print(y)
